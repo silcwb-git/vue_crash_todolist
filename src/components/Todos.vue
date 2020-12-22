298a94ff4bcc9142275f8f824bb0a0b6b1e7b715
A0112039 <silvana.siqueira@telefonica.com>
2020-12-21 23:40:28 -0300

    added component TodoItem

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <div v-for="todo in todos" v-bind:key="todo">
-        <h3>{{todo.title}}</h3>
+    <div v-bind:key="todo.id" v-for="todo in todos" >
+        <TodoItem v-bind:todo="todo" />
      </div>
   </div> 
 </template>
 
 <script>
+
+import TodoItem from './TodoItem';
+
 export default {
     name: "Todos",
+    components: {
+      TodoItem
+    },
     props: ["todos"]
 }
 </script>
